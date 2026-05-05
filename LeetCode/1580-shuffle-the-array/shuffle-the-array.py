c4b43632a8c9a6bcc770a3f2ffc0f910ed5892ae
congrats-hg <hagyeong929@gmail.com>
2026-05-05 11:16:59 +0900

    Time: 63 ms (62.17%) | Memory: 19.4 MB (17.44%) - LeetSync

--- LeetCode/1580-shuffle-the-array/shuffle-the-array.py
+++ LeetCode/1580-shuffle-the-array/shuffle-the-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x_lst = nums[:n]
+        y_lst = nums[n:]
 
         ans = []
 
-        for x, y in zip(nums[:n], nums[n:]):
+        for x, y in zip(x_lst, y_lst):
             ans.append(x)
             ans.append(y)
         
