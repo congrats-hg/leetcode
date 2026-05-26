3353cf93d1d8e9ca10c96c5976c58c481cccdc6d
congrats-hg <hagyeong929@gmail.com>
2026-05-26 11:28:37 +0900

    Time: 0 ms (100.00%) | Memory: 19 MB (99.68%) - LeetSync

---
+++ LeetCode/3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        single = 0
+        double = 0
+
+        for num in nums:
+            if num < 10:
+                single += num
+            else:
+                double += num
+        
+        if single == double:
+            return False
+        
+        return True
